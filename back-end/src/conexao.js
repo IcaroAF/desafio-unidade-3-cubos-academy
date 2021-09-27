cd3ca9e5b6b36b98cc6fa094323422d9551abab9
icaroaf <caroassisfreitas@globo.com>
2021-09-27 17:58:50 -0300

    configuração da conexao do banco de dados da heroku

--- back-end/src/conexao.js
+++ back-end/src/conexao.js
@@ -1,11 +1,24 @@
 const { Pool } = require('pg');
 
+// config local
+// const pool = new Pool({
+//     user: 'postgres',
+//     host: 'localhost',
+//     database: 'market_cubos',
+//     password: 'postgres',
+//     port: 5432
+// });
+
+//config heroku
 const pool = new Pool({
-    user: 'postgres',
-    host: 'localhost',
-    database: 'market_cubos',
-    password: 'postgres',
-    port: 5432
+    user: 'tlqmdkowtoxczb',
+    host: 'ec2-34-197-135-44.compute-1.amazonaws.com',
+    database: 'd2k1tvt42206ov',
+    password: '9608fde12c1e18b04675058d6e18e151b1ec8d4f331dc82c3aa584a0b52bdf63',
+    port: 5432,
+    ssl: {
+        rejectUnauthorized: false
+    }
 });
 
 const query = (text, param) => {
