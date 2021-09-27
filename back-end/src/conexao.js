e9ea9db41b033338378cfbe1fc29941c75a69184
icaroaf <caroassisfreitas@globo.com>
2021-09-27 20:12:35 -0300

    configuracao database heroku

--- back-end/src/conexao.js
+++ back-end/src/conexao.js
@@ -13,7 +13,7 @@ const { Pool } = require('pg');
 const pool = new Pool({
     user: 'nmraumyfybqytx',
     host: 'ec2-34-233-0-64.compute-1.amazonaws.com',
-    database: 'd2k1tvt42206ov',
+    database: 'd2ttnnkgd2fot',
     password: 'd29ccd7b6c8363dd8b20b85c8ba74a567e5c9b75cc05b0571763bebfeeb86a96',
     port: 5432,
     ssl: {
